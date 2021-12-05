5696b1bf3263bccaf142e5359ebb8c5924be4154
elfgod <albertogx@icloud.com>
2021-12-04 21:35:00 -0500

    AppBar Transitions

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 
-const Header = () => {
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+} 
+
+
+const Header = (props) => {
   return (
-    <AppBar position='fixed'>
-      <Toolbar>
-        AlienRoom Instituto
-      </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position='fixed'>
+        <Toolbar>
+          AlienRoom Instituto
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   )
 }
 
