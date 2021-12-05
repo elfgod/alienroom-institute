d59b52f99b2406b3b73d341ecf71d5be937b6670
elfgod <albertogx@icloud.com>
2021-12-04 22:11:08 -0500

    Styling - Themes (Palette)

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -1,13 +1,20 @@
 import { createTheme } from '@mui/material/styles';
 import { green, purple } from '@mui/material/colors';
 
+const alienPurple = '#320357'
+const alienAqua = '#00ffff'
+
 const theme = createTheme({
   palette: {
+    common: {
+      purple: `${alienPurple}`,
+      aqua: `${alienAqua}`
+    },
     primary: {
-      main: purple[500],
+      main: `${alienPurple}`,
     },
     secondary: {
-      main: green[500],
+      main: `${alienAqua}`,
     },
   },
 });
