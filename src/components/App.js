02148642c50a304975d70322b9e5d9d0c8e36c61
elfgod <albertogx@icloud.com>
2021-12-04 21:31:03 -0500

    The AppBar Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../components/ui/Header'
 
 const App = () => {
   return (
     <>
+    <Header />
     <div>
       <h1>AlienRoom Instituto</h1>
     </div>      
---
+++ src/components/ui/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+const Header = () => {
+  return (
+    <AppBar position='fixed'>
+      <Toolbar>
+        AlienRoom Instituto
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default Header
