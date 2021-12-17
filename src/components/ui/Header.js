6377992c34edb557b75f785422acb2ad15cf4959
elfgod <albertogx@icloud.com>
2021-12-16 20:23:48 -0500

    AppBar Logo

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,8 +2,9 @@ import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
-import { Typography } from '@mui/material';
 import { makeStyles } from "@mui/styles"
+
+import logo from '../../assets/logo.svg'
 
 function ElevationScroll(props) {
   const { children } = props;
@@ -20,7 +21,12 @@ function ElevationScroll(props) {
 
   const useStyles = makeStyles(theme => ({
     toolbarMargin: {
-      ...theme.mixins.toolbar
+      ...theme.mixins.toolbar,
+      marginBottom: "3em"
+    },
+    logo: {
+      height: "3.5em",
+      margin: "0.5em"
     }
 }))
 
@@ -31,10 +37,8 @@ export default function Header(props) {
     <>
       <ElevationScroll>
         <AppBar position='fixed'>
-          <Toolbar>
-            <Typography variant='h5' color='secondary'> 
-              AlienRoom
-            </Typography>
+          <Toolbar disableGutters>
+            <img alt="company logo" className={classes.logo} src={logo} />
           </Toolbar>
         </AppBar>
       </ElevationScroll>
