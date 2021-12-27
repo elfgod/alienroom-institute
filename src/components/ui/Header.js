7f7ac74fd7bbfac8057b5e2157ffa9320cf8115d
elfgod <albertogx@icloud.com>
2021-12-26 21:16:02 -0500

    Active Tab Refresh Fix

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
@@ -32,6 +32,12 @@ const useStyles = makeStyles(theme => ({
     height: '3.5em',
     margin: '0.5em'
   },
+  logoContainer: {
+    padding: 0,
+    '&:hover': {
+      backgroundColor: 'transparent'
+    }
+  },
   tabContainer: {
     marginLeft: 'auto'
   },
@@ -57,6 +63,20 @@ export default function Header(props) {
     setValue(value);
   }
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/courses' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/services' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contactus' && value !== 4) {
+      setValue(4);
+    }
+  }, [value]);
+
   return (
     <>
       <ElevationScroll>
@@ -70,25 +90,32 @@ export default function Header(props) {
         fontWeight: '700',
       }}>
           <Toolbar disableGutters>
-            <img alt="company logo" className={classes.logo} src={logo} />
-          <Tabs
-            value={value}
-            className={classes.tabContainer}
-            onChange={handleChange}
-            indicatorColor='success'
-          >
-            <Tab className={classes.tab} component={Link} to='/' label="Home" />
-            <Tab className={classes.tab} component={Link} to='/coures' label="Courses" />
-            <Tab className={classes.tab} component={Link} to='/services' label="Services" />
-            <Tab className={classes.tab} component={Link} to='/about' label="Why AlienRoom" />
-            <Tab className={classes.tab} component={Link} to='/contactus' label="Contact Us" />
-          </Tabs>
-          <Button
-            variant="contained"
-            color="secondary"
-            className={classes.button}>
+            <Button
+              component={Link}
+              disableRipple
+              to='/' className={classes.logoContainer}
+              onClick={() => setValue(0)}
+            >
+              <img alt="company logo" className={classes.logo} src={logo} />
+            </Button>
+            <Tabs
+              value={value}
+              className={classes.tabContainer}
+              onChange={handleChange}
+              indicatorColor='success'
+            >
+              <Tab className={classes.tab} component={Link} to='/' label="Home" />
+              <Tab className={classes.tab} component={Link} to='/courses' label="Courses" />
+              <Tab className={classes.tab} component={Link} to='/services' label="Services" />
+              <Tab className={classes.tab} component={Link} to='/about' label="Why AlienRoom" />
+              <Tab className={classes.tab} component={Link} to='/contactus' label="Contact Us" />
+            </Tabs>
+            <Button
+              variant="contained"
+              color="secondary"
+              className={classes.button}>
               Free Estimated
-          </Button>          
+            </Button>          
           </Toolbar>
         </AppBar>
       </ElevationScroll>
